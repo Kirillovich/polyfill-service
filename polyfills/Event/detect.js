134a6f5996d66fe4fc3e4aa0c0ca2b6718a35cc9
Jake Champion <jakechampion.jake2@googlemail.com>
2017-09-04 23:11:14 +0100

    Update detect to account for the EventListenerOptions parameter

--- polyfills/Event/detect.js
+++ polyfills/Event/detect.js
@@ -1,13 +1,21 @@
 (function(global) {
 
 	if (!('Event' in global)) return false;
-	if (typeof global.Event === 'function') return true;
 
 	try {
 
 		// In IE 9-11, the Event object exists but cannot be instantiated
 		new Event('click');
-		return true;
+		var supportEventListenerOptions = false;
+		try {
+			document.createElement('div').addEventListener('test', null, Object.defineProperty({}, 'capture', {
+				get: function () {
+					supportEventListenerOptions = true;
+				}
+			}));
+		} finally {
+			return supportEventListenerOptions;
+		}
 	} catch(e) {
 		return false;
 	}
